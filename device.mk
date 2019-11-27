147c98128a58fed7e30243a0b53b07658c2993a8
facoco <facoco@gmx.net>
2019-11-26 20:02:14 -0600

    Build Camera2

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    Snap
+    Camera2
 
 # Shims
 PRODUCT_PACKAGES += \
